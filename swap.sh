cb1e5e38874036dd567ed0e6d74e43c6bdf898a9
ali <binbinxm@gmail.com>
2021-01-04 13:48:39 +0800

    remove homeassist

--- swap.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-echo "creating directory for swapfile, /var/swap"
-mkdir -p /var/swap
-echo "creating 4GB swapfile..."
-dd if=/dev/zero of=/var/swap/swapfile bs=1M count=4096
-mkswap /var/swap/swapfile
-swapon /var/swap/swapfile
-echo "/var/swap/swapfile swap swap defaults 0 0" >> /etc/fstab
-free -h
